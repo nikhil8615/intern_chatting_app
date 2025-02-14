d4f43045edc2967850ae4c1482310cee34896cdd
Nikhil Singh <nikhilbdps1256@gmail.com>
2025-02-14 17:10:08 +0530

    solved

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002",
+        target: "https://intern-chatting-app.onrender.com/",
         changeOrigin: true,
       },
     },
